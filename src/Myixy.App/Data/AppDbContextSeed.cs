d2836fd11ac4b60f8e4e303c22a3abb36eccec60
Shiming Ge <v-shige@microsoft.com>
2018-09-29 11:05:55 +0800

    add seed

--- src/Myixy.App/Data/AppDbContextSeed.cs
+++ src/Myixy.App/Data/AppDbContextSeed.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Myixy.App.Areas.Identity.Data;
+using NLog.Web;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,19 +18,34 @@ namespace Myixy.App.Data
 
             using (IServiceScope scope = scopeFactory.CreateScope())
             {
-                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
-                var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<MyixyUser>>();
+                var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+                var email = "geshiming@outlook.com";
+                var password = "1qaz@WSX";
+
+                var user = new MyixyUser { UserName = email, Email = email };
+                var result = await userManager.CreateAsync(user, password);
+                if(result.Succeeded)
+                {
+                    logger.Log(NLog.LogLevel.Info, "Add seed succeeded");
+                }
+                else
+                {
+                    logger.Log(NLog.LogLevel.Warn, "Add seed succeeded");
+                }
+
+                //var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
 
                 // User Info
                 //string userName = "myixy";
                 //string email = "myixy666@gmail.com";
                 //string password = "mySecur1ty!";
-                string role = "SuperAdmin";
+                //string role = "SuperAdmin";
 
-                if (await roleManager.FindByNameAsync(role) == null)
-                {
-                    await roleManager.CreateAsync(new IdentityRole(role));
-                }
+                //if (await roleManager.FindByNameAsync(role) == null)
+                //{
+                //    await roleManager.CreateAsync(new IdentityRole(role));
+                //}
 
                 //if (await userManager.FindByNameAsync(userName) == null)
                 //{
